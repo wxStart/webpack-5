78aeab5909a08c80f1d6bcbdcf6b1a2090874192
wangxing <xing.wang@xforwardai.com>
2023-06-06 17:45:18 +0800

    [important]:代码检测 和 js语法转换低版本语法

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 const count = 2;
+console.log("count: ", count);
+const ab = () => {
+  const a = [...[1, 2, 4]];
+  console.log("a: ", a);
+  const obj = {
+    foo: {
+      bar: {
+        baz() {
+          return 42;
+        },
+      },
+    },
+  };
+
+  const baz = obj?.foo?.bar?.baz(); // 42
+  console.log('baz: ', baz);
+};
+ab();
+// var a = 12;
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 const path = require("path");
-
+const ESLintPlugin = require("eslint-webpack-plugin");
+const HtmllPlugin = require("html-webpack-plugin");
 const config = {
   entry: "./src/main.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    
   },
   module: {
     rules: [
-    //   {
-    //     test: /\.js$/,
-    //     loader: "babel-loader",
-    //   },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
     ],
   },
-  plugins: [],
+  plugins: [
+    new ESLintPlugin({
+      context: path.resolve(__dirname, "src"), // 检测src的下面的文件
+    }),
+    new HtmllPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
   mode: "development",
 };
 
